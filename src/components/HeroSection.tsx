2a2ee8c1a4b0883de544d12440a55fb7163a914f
Atjebel <noone.a10@gamil.com>
2026-04-24 10:51:51 +0300

    4 commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,10 +3,10 @@ import { Globe2, ShieldCheck, Headphones, Sparkles } from "lucide-react";
 import { useState, useEffect } from "react";
 import { useLanguage } from "@/context/LanguageContext";
 
-import img1 from "@/assets/pexels-tariqul-34868593.jpg";
-import img2 from "@/assets/airplane-taking-off-sunset-scene-generative-ai.jpg";
-import img3 from "@/assets/pexels-planespotter-geneva-1877406873-36897685.jpg";
-import img4 from "@/assets/view-air-plane-wing-take-off-landing.jpg";
+import img1 from "@/assets/1.jpg";
+import img2 from "@/assets/2.jpg";
+import img3 from "@/assets/3.jpg";
+import img4 from "@/assets/4.jpg";
 
 const heroImages = [img1, img2, img3, img4];
 
