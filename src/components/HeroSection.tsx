a7be13e6d0c6221b55e6a5ec31b00ff49cc1ce01
Atjebel <noone.a10@gamil.com>
2026-04-24 12:20:25 +0300

    5commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,10 +5,9 @@ import { useLanguage } from "@/context/LanguageContext";
 
 import img1 from "@/assets/1.jpg";
 import img2 from "@/assets/2.jpg";
-import img3 from "@/assets/3.jpg";
 import img4 from "@/assets/4.jpg";
 
-const heroImages = [img1, img2, img3, img4];
+const heroImages = [img1, img2, img4];
 
 const HeroSection = () => {
   const [currentImgIndex, setCurrentImgIndex] = useState(0);
